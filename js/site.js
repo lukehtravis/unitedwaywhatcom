7b29a1c8d0752722dccfa9c2e32478d280a1bc7d
luke <lukehtravis@gmail.com>
2018-07-26 11:19:01 -0700

    improved js functionality for menus

--- js/site.js
+++ js/site.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-  $('.main-menu > ul > li').on('click', function() {
-    $(this).find('ul > li').toggle();
+  $('.main-menu > ul > li').mouseenter(function() {
+    $('.level-2, .arrow-up').removeClass('selected');
+    $(this).find('ul, .arrow-up').addClass('selected');
+  })
+  $('.main-menu > ul > li').mouseleave(function(){
+    $(this).find('ul, .arrow-up').removeClass('selected');
   })
 })
