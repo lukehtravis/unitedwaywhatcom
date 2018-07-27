0bea3fc134309005e6e9aaaf2d47036324bddbef
luke <lukehtravis@gmail.com>
2018-07-27 10:26:36 -0700

    added php files

---
+++ footer.php
@@ -0,0 +1,68 @@
+<footer>
+  <div class="footer-container">
+    <section class="top-footer">
+      <div class="footer-title">
+        <span>United Way Of Whatcom County</span>
+      </div>
+      <div class="footer-contact">
+        <img src="assets/img/Phone.png" /><span>32883288</span>
+        <span>Email Us</span>
+        <span>Connect With Us</span>
+      </div>
+    </section>
+    <section class="bottom-footer">
+      <div class="footer-menus">
+        <div class="footer-menu-container">
+          <h3>Menu header</h3>
+          <ul class="footer-menu">
+            <li>Menu Item</li>
+            <li>Menu Item</li>
+            <li>Menu Item</li>
+          </ul>
+        </div>
+        <div class="footer-menu-container">
+          <h3>Menu header</h3>
+          <ul class="footer-menu">
+            <li>Menu Item</li>
+            <li>Menu Item</li>
+            <li>Menu Item</li>
+          </ul>
+        </div>
+        <div class="footer-menu-container">
+          <h3>Menu header</h3>
+          <ul class="footer-menu">
+            <li>Menu Item</li>
+            <li>Menu Item</li>
+            <li>Menu Item</li>
+          </ul>
+        </div>
+        <div class="footer-menu-container">
+          <h3>Menu header</h3>
+          <ul class="footer-menu">
+            <li>Menu Item</li>
+            <li>Menu Item</li>
+            <li>Menu Item</li>
+          </ul>
+        </div>
+      </div>
+      <div class="footer-credits">
+        <div class="address">
+          <p>
+            Part of the Whatcom Center for Philanthropy
+            (Top floor of the Washington Federal Building)
+            1500 Cornwall Ave Ste 203
+            Bellingham, WA 98225-4579
+          </p>
+          <a>See Map</a>
+        </div>
+        <div>
+          <img />
+          <p>Credits</p>
+        </div>
+      </div>
+    </section>
+  </div>
+</footer>
+<script type="text/javascript" src="assets/js/site.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<?php
+  $root_path = $_SERVER['DOCUMENT_ROOT'] . "/unitedwaywhatcom";
+  include $root_path . "/header.php";
+?>
+    <section class="banner"><!-- Placeholder for body content --></section>
+    <section class="content"><!-- Placeholder for body content --></section>
+<?php
+  include $root_path . "/footer.php";
+?>
