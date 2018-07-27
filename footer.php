4b18eef54c91d84828890b0d6151b51f82ef08c7
luke <lukehtravis@gmail.com>
2018-07-27 12:41:09 -0700

    desktop footer additions

--- footer.php
+++ footer.php
@@ -5,9 +5,10 @@
         <span>United Way Of Whatcom County</span>
       </div>
       <div class="footer-contact">
-        <img src="assets/img/Phone.png" /><span>32883288</span>
-        <span>Email Us</span>
-        <span>Connect With Us</span>
+        <div class="contact"><img src="assets/img/Phone.png" /><span>32883288</span></div>
+        <div class="contact"><img src="assets/img/email.png" /><span>Email Us</span></div>
+        <div class="contact"><img src="assets/img/facebook.png" /><span>Connect With Us</span></div>
+        <div class="contact"><img src="assets/img/newspaper.png" /><span>Get Our Newsletter</span></div>
       </div>
     </section>
     <section class="bottom-footer">
@@ -15,33 +16,57 @@
         <div class="footer-menu-container">
           <h3>Menu header</h3>
           <ul class="footer-menu">
-            <li>Menu Item</li>
-            <li>Menu Item</li>
-            <li>Menu Item</li>
+            <li>
+              <a href="#">Menu Item</a>
+              <ul class="footer-submenu">
+                <li><a href="#">Submenu Item</a>
+                <li><a href="#">Submenu Item</a>
+              </ul>
+            </li>
+            <li><a href="#">Menu Item</a></li>
+            <li><a href="#">Menu Item</a></li>
           </ul>
         </div>
         <div class="footer-menu-container">
           <h3>Menu header</h3>
           <ul class="footer-menu">
-            <li>Menu Item</li>
-            <li>Menu Item</li>
-            <li>Menu Item</li>
+            <li>
+              <a href="#">Menu Item</a>
+              <ul class="footer-submenu">
+                <li><a href="#">Submenu Item</a>
+                <li><a href="#">Submenu Item</a>
+              </ul>
+            </li>
+            <li><a href="#">Menu Item</a></li>
+            <li><a href="#">Menu Item</a></li>
           </ul>
         </div>
         <div class="footer-menu-container">
           <h3>Menu header</h3>
           <ul class="footer-menu">
-            <li>Menu Item</li>
-            <li>Menu Item</li>
-            <li>Menu Item</li>
+            <li>
+              <a href="#">Menu Item</a>
+              <ul class="footer-submenu">
+                <li><a href="#">Submenu Item</a>
+                <li><a href="#">Submenu Item</a>
+              </ul>
+            </li>
+            <li><a href="#">Menu Item</a></li>
+            <li><a href="#">Menu Item</a></li>
           </ul>
         </div>
         <div class="footer-menu-container">
           <h3>Menu header</h3>
           <ul class="footer-menu">
-            <li>Menu Item</li>
-            <li>Menu Item</li>
-            <li>Menu Item</li>
+            <li>
+              <a href="#">Menu Item</a>
+              <ul class="footer-submenu">
+                <li><a href="#">Submenu Item</a>
+                <li><a href="#">Submenu Item</a>
+              </ul>
+            </li>
+            <li><a href="#">Menu Item</a></li>
+            <li><a href="#">Menu Item</a></li>
           </ul>
         </div>
       </div>
@@ -52,11 +77,10 @@
             (Top floor of the Washington Federal Building)
             1500 Cornwall Ave Ste 203
             Bellingham, WA 98225-4579
+            <a href="#">See Map</a>
           </p>
-          <a>See Map</a>
         </div>
         <div>
-          <img />
           <p>Credits</p>
         </div>
       </div>
