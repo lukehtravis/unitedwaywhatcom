f7a5b39978d26a8e9807e6e7aac63b59a60a8929
Larry <github@lroot.com>
2018-08-04 11:53:09 -0700

    created jump page for easier review of completed templates/pages

--- index.php
+++ index.php
@@ -1,17 +1,42 @@
-<?php require_once __DIR__.'/inc/bootstrap.php'; ?>
 <!doctype html>
-<?php
-  include $root_path . '/inc/header.php';
-?>
-    <?php
-      // include $root_path . '/inc/home.php';
-      // include $root_path . '/inc/events.php';
-       include $root_path . '/inc/get-involved.php';
-      // include $root_path . '/inc/donations.php';
-      // include $root_path . '/inc/get-involved-calc.php';
-      // include $root_path . '/inc/donations-complete.php';
-      // include $root_path . '/inc/oops.php';
-    ?>
-<?php
-  include $root_path . '/inc/footer.php';
-?>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>The HTML5 Herald</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<h1>UWW UI Templates</h1>
+<nav>
+    <ul>
+        <li><a href="/0.0-home.php">0.0 Home</a></li>
+        <li>1.0 Our Work / What We Do</li>
+        <li>1.1 Our Work/ What We Do / Our Network</li>
+        <li>1.2 Our Work / How We Do It</li>
+        <li><a href="/2.0-get-involved-give.php">2.0 Get Involved / Give</a></li>
+        <li><a href="/2.1-get-involved-give-impact-calc.php">2.1 Get Involved / Give / Impact Calc</a></li>
+        <li>2.2 Get Involved / Give / Workplace Campaigns</li>
+        <li>2.3 Get Involved / Give / Personal Donations</li>
+        <li>2.4 Get Involved / Advocate</li>
+        <li>2.5 Get Involved / Volunteer</li>
+        <li>3.0 Get Help / Help for Organizations</li>
+        <li>3.1 Get Help / Help for People</li>
+        <li><a href="/4.0-donate.php">4.0 Donate</a></li>
+        <li><a href="/4.1-donate-complete.php">4.1 Donate / Complete</a></li>
+        <li><a href="/5.0-other-events.php">5.0 Other / Events</a></li>
+        <li>5.1 Other / Staff</li>
+        <li>5.2 Other / Board</li>
+        <li>5.3 Other / Fund Distribution Committee</li>
+        <li>5.4 Other / Partner Agencies</li>
+        <li>5.5 Other / Financials</li>
+        <li>5.6 Other / Job Openings</li>
+        <li>5.7 Other / FAQs</li>
+        <li>6.0 Blog (Listing)</li>
+        <li>6.1 Blog / Post</li>
+        <li>7.0 Logged In / Board Members Page</li>
+        <li>7.1 Logged In / Volunteers Page</li>
+        <li><a href="/_oops.php">Oops</a></li>
+    </ul>
+</nav>
+</body>
+</html>
