7725c9ca9c72d4cca72c55013514c32642c2261d
luke <lukehtravis@gmail.com>
2018-08-08 13:03:11 -0700

    edits to our work page

--- inc/1.2-our-work.php
+++ inc/1.2-our-work.php
@@ -1,13 +1,42 @@
-<section class="banner our-work">
-  <div class="headline">
-    <h1>How We Do It</h1>
-    <span class="black-bg">We Do It For You</span>
-    <span class="black-bg">We Do It With You</span>
-  </div>
-</section>
-<section class="body-overlap">
-  <div class="top-text">
-    <h2>We are a Non-Profit that Supports Non Profits</h2>
-    <p>Here’s how it works: You donate to United Way of Whatcom County. We carefully vet non-profits and allocate funds. Those non-profits provide services to people in need.</p>
-  </div>
-</section>
+<div class="color-container orange-bg">
+  <section class="banner our-work">
+    <div class="headline">
+      <h1>How We Do It</h1>
+      <span class="black-bg">We Do It For You</span>
+      <span class="black-bg">We Do It With You</span>
+    </div>
+  </section>
+  <section class="body-overlap">
+    <div class="top-text">
+      <h2>We are a Non-Profit that Supports Non Profits</h2>
+      <p>Here’s how it works: You donate to United Way of Whatcom County. We carefully vet non-profits and allocate funds. Those non-profits provide services to people in need.</p>
+    </div>
+    <div class="icon-grid">
+      <div class="icon-column">
+        <img src="/assets/img/Dollar.png" />
+        <h3>Generous people donate money</h3>
+        <p>Individuals donate directly to United Way  and volunteers help us raise money through our unique workplace giving campaigns. The money goes into our Community Impact Fund. Whatcom County is our home. We are all responsible for making it a better place to live.</p>
+        <a href="#"></a>
+      </div>
+      <div class="icon-column">
+        <img src="/assets/img/Review.png" />
+        <h3>Our comittee meets & reviews</h3>
+        <p>Dozens of non-profit programs approach us with ideas for how they can help move the needle on poverty. Each year, our Fund Distribution Committee, made up of 30-50 volunteers, vets these programs. Our Community Impact Fund gets distributed to the combination of programs determined to best meet the needs of Whatcom County residents. Currently, we fund over 30 programs.</p>
+        <a href="#"></a>
+      </div>
+      <div class="icon-column">
+        <img src="/assets/img/Send.png" />
+        <h3>The money is sent out to non-profits</h3>
+        <p>The money is distributed to hardworking non-profits that provide services to the community. Our funded programs are held accountable for the work they do. They provide outcomes on their results to us at least annually.</p>
+        <p>Throughout the year, we convene our partners, along with the public sector and anyone else interested in our issue. We look for new ways to solve old problems. We look for ways to be more efficient. More collaborative. More inventive.</p>
+        <a href="#"></a>
+      </div>
+      <div class="icon-column">
+        <img src="/assets/img/Plate.png" />
+        <h3>Those in need get services</h3>
+        <p>We all live better. Every year, tens of thousands of individuals and families in Whatcom County receive support from the non-profit partner agencies we fund.</p>
+        <a href="#"></a>
+      </div>
+    </div>
+  </section>
+</div>
