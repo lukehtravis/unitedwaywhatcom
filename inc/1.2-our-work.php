990cc433df1e294d042820dd6cd487c1a5cb32a5
luke <lukehtravis@gmail.com>
2018-08-08 14:52:35 -0700

    added caption our work

--- inc/1.2-our-work.php
+++ inc/1.2-our-work.php
@@ -13,6 +13,7 @@
     </div>
   </section>
   <section class="body-overlap">
+    <span class="banner-caption">Sheena Oldham, a beneficiary of United Way funded programs</span>
     <div class="top-text">
       <h2>We are a Non-Profit that Supports Non Profits</h2>
       <p>Here’s how it works: You donate to United Way of Whatcom County. We carefully vet non-profits and allocate funds. Those non-profits provide services to people in need.</p>
