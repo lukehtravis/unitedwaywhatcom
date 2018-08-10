3592d0c5bef6263d645a7f11b90255f3f9b3706d
luke <lukehtravis@gmail.com>
2018-08-10 14:39:03 -0700

    desktop volunteer page

--- inc/get-involved-volunteer.php
+++ inc/get-involved-volunteer.php
@@ -8,5 +8,79 @@
     </div>
   </section>
   <section class="body-overlap">
+    <div class="volunteer-section">
+      <div class="volunteer-row">
+        <span>Fund Distribution Committe</span>
+        <div class="volunteer-content">
+          <p>We believe decisions for our community should be made by our community. Our fund allocation process is entirely driven by local residents. This volunteer committee reviews all programs applying for funding and gives recommendations for grants to the Board of Directors. They also review existing partnerships to ensure those partnerships are doing their part to make a positive impact on the lives of individuals and families who struggle financially in Whatcom County.</p>
+          <div class="volunteer-details">
+            <div class="volunteer-detail">
+              <span>Calendar:</span><p>April - May</p>
+            </div>
+            <div class="volunteer-detail">
+              <span>Time Commitment:</span><p>15-20 hours</p>
+            </div>
+            <div class="volunteer-detail">
+              <span>Volunteer Count:</span><p>30 - 50</p>
+            </div>
+            <p><b>Interested?</b> <a href="#">Contact Us</a> to learn more.</p>
+          </div>
+        </div>
+      </div>
+      <div class="volunteer-row">
+        <span>Fund Distribution Committe</span>
+        <div class="volunteer-content">
+          <p>We believe decisions for our community should be made by our community. Our fund allocation process is entirely driven by local residents. This volunteer committee reviews all programs applying for funding and gives recommendations for grants to the Board of Directors. They also review existing partnerships to ensure those partnerships are doing their part to make a positive impact on the lives of individuals and families who struggle financially in Whatcom County.</p>
+          <div class="volunteer-details">
+            <div class="volunteer-detail">
+              <span>Calendar:</span><p>Variable: Determined By Workplace</p>
+            </div>
+            <div class="volunteer-detail">
+              <span>Time Commitment:</span><p>15-20 hours</p>
+            </div>
+            <div class="volunteer-detail">
+              <span>Volunteer Count:</span><p>30 - 50</p>
+            </div>
+            <p><b>Interested?</b> <a href="#">Contact Us</a> to learn more.</p>
+          </div>
+        </div>
+      </div>
+      <div class="volunteer-row">
+        <span>Fund Distribution Committe</span>
+        <div class="volunteer-content">
+          <p>We believe decisions for our community should be made by our community. Our fund allocation process is entirely driven by local residents. This volunteer committee reviews all programs applying for funding and gives recommendations for grants to the Board of Directors. They also review existing partnerships to ensure those partnerships are doing their part to make a positive impact on the lives of individuals and families who struggle financially in Whatcom County.</p>
+          <div class="volunteer-details">
+            <div class="volunteer-detail">
+              <span>Calendar:</span><p>Variable: Determined By Workplace</p>
+            </div>
+            <div class="volunteer-detail">
+              <span>Time Commitment:</span><p>15-20 hours</p>
+            </div>
+            <div class="volunteer-detail">
+              <span>Volunteer Count:</span><p>30 - 50</p>
+            </div>
+            <p><b>Interested?</b> <a href="#">Contact Us</a> to learn more.</p>
+          </div>
+        </div>
+      </div>
+      <div class="volunteer-row">
+        <span>Fund Distribution Committe</span>
+        <div class="volunteer-content">
+          <p>We believe decisions for our community should be made by our community. Our fund allocation process is entirely driven by local residents. This volunteer committee reviews all programs applying for funding and gives recommendations for grants to the Board of Directors. They also review existing partnerships to ensure those partnerships are doing their part to make a positive impact on the lives of individuals and families who struggle financially in Whatcom County.</p>
+          <div class="volunteer-details">
+            <div class="volunteer-detail">
+              <span>Calendar:</span><p>Variable: Determined By Workplace</p>
+            </div>
+            <div class="volunteer-detail">
+              <span>Time Commitment:</span><p>15-20 hours</p>
+            </div>
+            <div class="volunteer-detail">
+              <span>Volunteer Count:</span><p>30 - 50</p>
+            </div>
+            <p><b>Interested?</b> <a href="#">Contact Us</a> to learn more.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </div>
