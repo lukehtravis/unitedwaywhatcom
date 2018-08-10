bcfcc3f96ca089c5788e214f200f0ebc2c8bbaa2
luke <lukehtravis@gmail.com>
2018-08-09 23:11:08 -0700

    file naming switch

--- inc/get-help-people.php
+++ inc/get-help-people.php
@@ -1,22 +1,64 @@
-<div class="color-container darkblue-bg get-help-people">
+<div class="color-container darkblue-bg get-help">
   <section class="banner get-help">
     <div class="headline">
-      <h1>Help For Organizations</h1>
-      <span class="lightblue-bg">We're In</span>
-      <span class="lightblue-bg">Thris Together</span>
-      <span class="orange-bg small-headline">Live United</span>
+      <h1>How We Do It</h1>
+      <span class="lightblue-bg">Not Just Survive</span>
+      <span class="lightblue-bg">Thrive</span>
+      <p>Beyond  funding our local Partner Agencies, we offer the following programs directly to people in need: </p>
     </div>
   </section>
   <section class="body-overlap">
     <div class="column-half">
       <div class="body-section">
-        <h2>Become a partner</h2>
-        <p>If your non-profit program helps families and individuals increase their financial security, consider applying for a grant from our Community Impact Fund. Our funded programs are more than just grant recipients. We expect participation throughout the year. Joining our network means living United. We’re in this together.</p>
-        <a href="#"><button class="orange-bg">See Our 2018 Partner Agencies</button></a>
+        <h2>FamilyWize</h2>
+        <p>Healthcare is expensive. Over 80 million Americans are uninsured or underinsured. They can’t afford meds. 14% of people don't get their prescriptions filled.</p>
+        <p>As a Whatcom County resident, we don’t just want you to survive. We want you to thrive. Decreasing your prescription drug payments can help with that. We’ve teamed up with the FamilyWize® Community Service Partnership, Inc. to make it possible to get your meds and pay for the other stuff you need, like lunch and textbooks.</p>
+        <p>Everyone is eligible. FamilyWize prescription drug discount cards have zero qualifying limitations. No usage, age, or income limits.</p>
+        <p>It’s easy to use. Simply give the card to your participating pharmacist with your prescription.  An instant discount (varies by medication) is applied to the prescription medicine.</p>
+        <p>You’ll get the lowest price possible. The card provides pharmacy discounts (up to 75%) on all FDA approved prescription medicine not covered by insurance, Medicaid, Medicare, or other benefit plans.  You pay either the discounted price or the pharmacy's retail price, whichever is lower.</p>
+        <a href="#"><button class="orange-bg">Download Prescription Card</button></a>
       </div>
       <div class="body-section embed-graphic">
         <h3>People Helped. Money Saved</h3>
         <embed />
+      </div>
+    </div>
+    <div class="column-half">
+      <div class="body-section">
+        <h2>Project Homeless Connect</h2>
+        <p>Project Homeless Connect (PHC) is an international initiative sponsored by the U.S. Interagency Council on Homelessness (USICH). It has been implemented in well over 700 communities across the United States, Canada, and Australia. Our local PHC was first adopted in 2007 during Phase 3 of the Whatcom County Plan to End Homelessness. United Way of Whatcom County continues as the fiscal agent.</p>
+        <p>This event is designed to reduce barriers to services. Services available for free during PHC include:</p>
+        <ul>
+          <li>dental exams and services</li>
+          <li>general medical exams</li>
+          <li>haircuts</li>
+          <li>vision and hearing screenings</li>
+          <li>housing intakes</li>
+          <li>DOL photo ID</li>
+          <li>foot care</li>
+          <li>employment screenings</li>
+          <li>health insurance assistance</li>
+          <li>legal and dispute resolution counsel</li>
+          <li>GAU and SSA help</li>
+          <li>pet care</li>
+          <li>massage and chiropractic treatment</li>
+          <li>literacy and library assistance</li>
+        </ul>
+        <a href="#"><button class="orange-bg">Attend The Next PHC Event</button></a>
+      </div>
+      <div class="body-section">
+        <img src="/assets/img/younglady.jpg" />
+      </div>
+    </div>
+    <div class="column-half">
+      <div class="bottom-cta">
+        <div class="bottom-cta-text">
+          <h2>Need more help?</h2>
+          <p>United Way of Whatcom County funds a network of local non-profits – we call them Partner Agencies – which provide services to those in need.</p>
+        </div>
+        <div class="bottom-cta-button">
+          <a href="#"><button class="orange-bg">See Our 2017 Partner Agencies</button></a>
+        </div>
       </div>
     </div>
   </section>
