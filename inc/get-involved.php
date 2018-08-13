e4612749dbea01f2e01504baa959065c5fc80492
luke <lukehtravis@gmail.com>
2018-08-12 18:29:33 -0700

    create container for larger pages

--- inc/get-involved.php
+++ inc/get-involved.php
@@ -1,41 +1,43 @@
-<section class="banner get-involved">
-  <img
-    srcset="
-      /assets/img/getinvolved.jpg 1400w,
-      /assets/img/getinvolved.jpg 893w,
-      /assets/img/getinvolved.jpg 535w
-    "
-    src="/assets/img/getinvolved.jpg"
-  />
-  <img
-    srcset="
-      /assets/img/getinvolvednohat.jpg 1400w,
-      /assets/img/getinvolvednohat.jpg 893w,
-      /assets/img/getinvolvednohat.jpg 535w
-    "
-    src="/assets/img/getinvolved.jpg"
-  />
-  <div class="headline">
-    <h1>Give</h1>
-    <span class="black-bg">You decided to Join</span>
-    <span class="black-bg">Us in the fight.</span>
-    <span class="darkorange-bg">Thank You.</span>
-  </div>
-  <div class="square-links">
-    <div class="square">
-      <img src="assets/img/impactcalcicon.png" />
-      <a href="#"><span>Impact Calculator</span></a>
-      <p>She used our funded services to get back on her feet. Now she’s paying it forward. See how Sheena broke the cycle.</p>
+<div class="color-container lightblue-bg get-involved">
+  <section class="banner get-involved">
+    <img
+      srcset="
+        /assets/img/getinvolved.jpg 1400w,
+        /assets/img/getinvolved.jpg 893w,
+        /assets/img/getinvolved.jpg 535w
+      "
+      src="/assets/img/getinvolved.jpg"
+    />
+    <img
+      srcset="
+        /assets/img/getinvolvednohat.jpg 1400w,
+        /assets/img/getinvolvednohat.jpg 893w,
+        /assets/img/getinvolvednohat.jpg 535w
+      "
+      src="/assets/img/getinvolved.jpg"
+    />
+    <div class="headline">
+      <h1>Give</h1>
+      <span class="black-bg">You decided to Join</span>
+      <span class="black-bg">Us in the fight.</span>
+      <span class="darkorange-bg">Thank You.</span>
     </div>
-    <div class="square">
-      <img src="assets/img/stickpeople.png" />
-      <a href="#"><span>Workplace Campaigns</span></a>
-      <p>She used our funded services to get back on her feet. Now she’s paying it forward. See how Sheena broke the cycle.</p>
+    <div class="square-links">
+      <div class="square">
+        <img src="assets/img/impactcalcicon.png" />
+        <a href="#"><span>Impact Calculator</span></a>
+        <p>She used our funded services to get back on her feet. Now she’s paying it forward. See how Sheena broke the cycle.</p>
+      </div>
+      <div class="square">
+        <img src="assets/img/stickpeople.png" />
+        <a href="#"><span>Workplace Campaigns</span></a>
+        <p>She used our funded services to get back on her feet. Now she’s paying it forward. See how Sheena broke the cycle.</p>
+      </div>
+      <div class="square">
+        <img src="assets/img/heartcash.png" />
+        <a href="#"><span>Personal Donations</span></a>
+        <p>She used our funded services to get back on her feet. Now she’s paying it forward. See how Sheena broke the cycle.</p>
+      </div>
     </div>
-    <div class="square">
-      <img src="assets/img/heartcash.png" />
-      <a href="#"><span>Personal Donations</span></a>
-      <p>She used our funded services to get back on her feet. Now she’s paying it forward. See how Sheena broke the cycle.</p>
-    </div>
-  </div>
-</section>
+  </section>
+</div>
