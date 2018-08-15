2fa134a591883b63a428ddad469d1422fd1c1be0
luke <lukehtravis@gmail.com>
2018-08-14 17:16:36 -0700

    our workplace desktop

--- inc/get-involved-workplace-campaigns.php
+++ inc/get-involved-workplace-campaigns.php
@@ -10,12 +10,14 @@
   <div class="normal-body">
     <div class="content-row">
       <h2>What is a workplace campaign?</h2>
+    </div>
+    <div class="content-row">
       <div class="small-side">
         <p>Here at United Way of Whatcom County, we conduct more than 150 workplace campaigns annually. During a workplace campaign, CEO’s and volunteer campaign coordinators encourage their employees and co-workers to donate to United Way of Whatcom County’s Community Impact Fund. Workplace campaigns are held at small businesses and large corporations in a variety of industries ranging from construction to banking. They are the backbone of our fundraising activities.</p>
       </div>
       <div class="large-side">
-        <p><b>A TYPICAL CAMPAIGN</b></p>
-        <ul>
+        <p class="special-margin-p"><b>A TYPICAL CAMPAIGN</b></p>
+        <ul class="bordered-list">
           <li>Someone (you?) at your place of work contacts us about hosting a campaign</li>
           <li>We help you plan your donation ask and include fun activities that build morale</li>
           <li>We set you up with an online portal to accept employee donations</li>
@@ -31,17 +33,24 @@
       <img src="/assets/img/nice-glasses.jpg" />
     </div>
     <div class="content-row">
-      <div class="small-side">
+      <div class="small-side stick-people">
         <img src="/assets/img/stickpeoplelarge.jpg" />
         <p class="small-text">Employees who give, and who see their employers give, feel good about their jobs. Employees who feel good about their jobs stick around.</p>
       </div>
       <div class="large-side">
-        <h3>STRENGTHEN WHATCOM COUNTY</h3>
-        <p>United Way of Whatcom County bands together the business, public, and social service sectors to achieve a common goal: financial stability for every person in our community.</p>
-        <h3>MAKE THINGS EASIER</h3>
-        <p>We help fund more than 30 local programs at over 25 non-profits. When we do the fundraising for them, their staff are freed up to provide direct services. We make fundraising easier for non-profits. We make giving easier for you. Why write 30 checks, when you can write one?</p>
-        <h3>BOOST YOUR REPUTATION</h3>
-        <p>When a company exhibits strong corporate social responsibility, the impact* on consumers is big:</p>
+        <h2>Why Host A Campaign?</h2>
+        <div class="spaced-text">
+          <h3>STRENGTHEN WHATCOM COUNTY</h3>
+          <p>United Way of Whatcom County bands together the business, public, and social service sectors to achieve a common goal: financial stability for every person in our community.</p>
+        </div>
+        <div class="spaced-text">
+          <h3>MAKE THINGS EASIER</h3>
+          <p>We help fund more than 30 local programs at over 25 non-profits. When we do the fundraising for them, their staff are freed up to provide direct services. We make fundraising easier for non-profits. We make giving easier for you. Why write 30 checks, when you can write one?</p>
+        </div>
+        <div class="spaced-text">
+          <h3>BOOST YOUR REPUTATION</h3>
+          <p>When a company exhibits strong corporate social responsibility, the impact* on consumers is big:</p>
+        </div>
         <div class="percent-strip">
           <div class="percent-section">
             <p class="percent-number">92%</p>
@@ -63,12 +72,12 @@
         <div>
           <h3>GET GOOD PEOPLE – AND KEEP THEM HAPPY</h3>
           <p>Millenials will soon make up 50% of the workforce. Here’s how they feel** about corporate social responsibility (CSR):</p>
-          <ul class="bordered-list">
-            <li>76% consider a company's CSR committments when deciding where to work</li>
-            <li>64% won’t take a job from a company that doesn’t exhibit strong CSR</li>
-            <li>88% say their job is more fulfilling when they have opportunities to make a positive impact on important issues</li>
-            <li>83% would be more loyal to a company that exhibits strong CSR</li>
-            <li>75% would take a pay cut to work for a socially responsible company</li>
+          <ul class="bordered-list unchecked">
+            <li><b>76%</b> consider a company's CSR committments when deciding where to work</li>
+            <li><b>64%</b> won’t take a job from a company that doesn’t exhibit strong CSR</li>
+            <li><b>88%</b> say their job is more fulfilling when they have opportunities to make a positive impact on important issues</li>
+            <li><b>83%</b> would be more loyal to a company that exhibits strong CSR</li>
+            <li><b>75%</b> would take a pay cut to work for a socially responsible company</li>
           </ul>
           <div class="asterisk-section">
             <p>*2017 Cone Communications CSR Study</p>
@@ -78,27 +87,25 @@
       </div>
     </div>
     <div class="content-row">
-      <div>
-        <p>2017</p>
-        <div>
-          <img src="/assets/img/blue-medal.png" />
+      <div class="award-box">
+        <div class="award-interior">
+          <p>2017</p>
           <div class="ribbon">
-            <p>Spirit of United Way Award</p>
+            <p class="ribbon-content">Spirit of United Way Award</p>
           </div>
-          <p>his award is given to businesses who are true champions of giving back. Their workplace campaigns show top-notch creativity, spirit, and engagement levels. They know how to raise the bar.</p>
+          <p class="middle-paragraph">his award is given to businesses who are true champions of giving back. Their workplace campaigns show top-notch creativity, spirit, and engagement levels. They know how to raise the bar.</p>
           <div class="white-box">
             <img src="/assets/img/company-logos.jpg" />
           </div>
         </div>
       </div>
-      <div>
-        <p>2017</p>
-        <div>
-          <img src="/assets/img/blue-medal.png" />
+      <div class="award-box">
+        <div class="award-interior">
+          <p>2017</p>
           <div class="ribbon">
-            <p>Spirit of United Way Award</p>
+            <p class="ribbon-content">Spirit of United Way Award</p>
           </div>
-          <p>his award is given to businesses who are true champions of giving back. Their workplace campaigns show top-notch creativity, spirit, and engagement levels. They know how to raise the bar.</p>
+          <p class="middle-paragraph">his award is given to businesses who are true champions of giving back. Their workplace campaigns show top-notch creativity, spirit, and engagement levels. They know how to raise the bar.</p>
           <div class="white-box">
             <img src="/assets/img/good-worker.jpg" />
           </div>
@@ -114,41 +121,39 @@
       </div>
       <div class="large-side">
         <div class="sponsor-table">
-          <div class="sponsor-level-column">
-            <div class="sponsor-row">
-              <img src="/assets/img/mountains.jpg" />
-              <p>Cascade</p>
-              <span>$20,000+</span>
-            </div>
-            <div class="sponsor-row">
-              <p>Mt. Baker</p>
-              <span>$10,000 - $19,999</span>
-            </div>
-            <div class="sponsor-row">
-              <p>Shuksan</p>
-              <span>$5,000 - $9,999</span>
-            </div>
-            <div class="sponsor-row">
-              <p>Chuckanut</p>
-              <span>$2,500 - $5,000</span>
-            </div>
+          <div class="sponsor-level-column largest darkblue-bg">
+            <div class="table-header">Sponsor Level</div>
+            <img src="/assets/img/mountains.jpg" />
+            <p>Cascade</p>
+            <span>$20,000+</span>
+          </div>
+          <div class="sponsor-name-column largest">
+            <div class="table-header">Sponsor Name</div>
+            <img src="/assets/img/sampson-logo.jpg" />
+            <a href="#">Samson Rope</a>
+          </div>
+          <div class="sponsor-level-column larger lightblue-bg">
+            <p>Mt. Baker</p>
+            <span>$10,000 - $19,999</span>
+          </div>
+          <div class="sponsor-name-column larger">
+            <p><a href="#">BP Cherry Point Refinery</a></p>
+            <p><a href="#">PeaceHealth St. Joseph Medical Center</a></p>
+            <p><a href="#">Whatcom Educational Credit Union</a></p>
+          </div>
+          <div class="sponsor-level-column lighterblue-bg">
+            <p>Shuksan</p>
+            <span>$5,000 - $9,999</span>
           </div>
           <div class="sponsor-name-column">
-            <div class="sponsor-row">
-              <img src="/assets/img/sampson-logo.jpg" />
-              <a href="#">Samson Rope</a>
-            </div>
-            <div class="sponsor-row">
-              <a href="#">BP Cherry Point Refinery</a>
-              <a href="#">PeaceHealth St. Joseph Medical Center</a>
-              <a href="#">Whatcom Educational Credit Union</a>
-            </div>
-            <div class="sponsor-row">
-              <a href="#">Boeing Corporation</a>
-            </div>
-            <div class="sponsor-row">
-              <a href="#">Power Tech Electric</a>
-            </div>
+            <a href="#">Boeing Corporation</a>
+          </div>
+          <div class="sponsor-level-column lightestblue">
+            <p>Chuckanut</p>
+            <span>$2,500 - $5,000</span>
+          </div>
+          <div class="sponsor-name-column">
+            <a href="#">Power Tech Electric</a>
           </div>
         </div>
       </div>
