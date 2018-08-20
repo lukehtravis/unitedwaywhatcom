28b891582e5671fe9dec18da8c5fd948cfc0032e
luke <lukehtravis@gmail.com>
2018-08-20 14:42:03 -0700

    other board page preview ready

---
+++ inc/other-board.php
@@ -0,0 +1,120 @@
+
+<div class="board-container">
+  <img src="/assets/img/meeting-table.png" class="board-photo" />
+  <section class="board">
+    <div class="board-intro">
+      <h1>Board of Directors</h1>
+      <p>Every United Way operates on its own in an independent capacity and is incorporated and governed by a dedicated group of local volunteers. Our Board of Directors is composed of volunteer corporate and community leaders in Whatcom County.</p>
+    </div>
+    <div class="board-box">
+      <div class="board-row">
+        <div class="board-details">
+          <div class="board-name-title">
+            <span class="board-name">Troy  Nutter</span>
+            <span class="board-title"> - Board Chair</span>
+          </div>
+          <p>Puget Sound Energy, Operational Training Manager</p>
+        </div>
+      </div>
+      <div class="board-row">
+        <div class="board-details">
+          <div class="board-name-title">
+            <span class="board-name">Debbie Hogue</span>
+            <span class="board-title"> - Past Board Chair</span>
+          </div>
+          <p>Peoples Bank, VP/Talent Acquisition Manager</p>
+        </div>
+      </div>
+      <div class="board-row">
+        <div class="board-details">
+          <div class="board-name-title">
+            <span class="board-name">Geoff Beaumont</span>
+
+          </div>
+          <p>Whatcom Transit Authority, Project Manager</p>
+        </div>
+      </div>
+      <div class="board-row">
+        <div class="board-details">
+          <div class="board-name-title">
+            <span class="board-name">Brent Belsher</span>
+
+          </div>
+          <p>Anvil Corp., Program Manager</p>
+        </div>
+      </div>
+      <div class="board-row">
+        <div class="board-details">
+          <div class="board-name-title">
+            <span class="board-name">Tony Bon</span>
+
+          </div>
+          <p>Retired</p>
+        </div>
+      </div>
+      <div class="board-row">
+        <div class="board-details">
+          <div class="board-name-title">
+            <span class="board-name">Lindsey Cerise</span>
+
+          </div>
+          <p>BP Cherry Point Refinery, Planning & Performance Reporting Manager</p>
+        </div>
+      </div>
+      <div class="board-row">
+        <div class="board-details">
+          <div class="board-name-title">
+            <span class="board-name"></span>
+
+          </div>
+          <p></p>
+        </div>
+      </div>
+      <div class="board-row">
+        <div class="board-details">
+          <div class="board-name-title">
+            <span class="board-name">Kara Irvin</span>
+
+          </div>
+          <p>US Bank, VP/Business Banking Team Manager</p>
+        </div>
+      </div>
+      <div class="board-row">
+        <div class="board-details">
+          <div class="board-name-title">
+            <span class="board-name">David Jones</span>
+
+          </div>
+          <p>Whatcom Educational Credit Union, Chief Marketing Officer</p>
+        </div>
+      </div>
+      <div class="board-row">
+        <div class="board-details">
+          <div class="board-name-title">
+            <span class="board-name">Cezar Mesquita</span>
+
+          </div>
+          <p>Western Washington University, Director of Admissions</p>
+        </div>
+      </div>
+      <div class="board-row">
+        <div class="board-details">
+          <div class="board-name-title">
+            <span class="board-name">Paul Murphy</span>
+
+          </div>
+          <p>Samson Rope Technologies, Director of Purchasing & Supply Chain</p>
+        </div>
+      </div>
+      <div class="board-row">
+        <div class="board-details">
+          <div class="board-name-title">
+            <span class="board-name">Jessica Sankey</span>
+
+          </div>
+          <p>Bellingham Public Schools, Director of Wellness</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
