60d3aa34eef2660d186f7c56e129bd5fb446853d
luke <lukehtravis@gmail.com>
2018-08-21 16:44:12 -0700

    added content for other fdc

--- inc/other-fdc.php
+++ inc/other-fdc.php
@@ -14,114 +14,235 @@
           <p class="red-text">Monday Panel Members</p>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Rebekah Duffy</span>
+              <span class="fdc-title"> - Chair</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>WECU</p>
           </div>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Tammy Bengen</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>Ferndale School District</p>
           </div>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Tony Bun</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>Community Member</p>
           </div>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Thomas Boucher</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>North Coast Credit Union</p>
           </div>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Carol Comeau</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>Community Volunteer</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Jack MacSlarrow</span>
+            </div>
+            <p>Community Volunteer</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Josh Nylander</span>
+            </div>
+            <p>Whatcom Transportation Authority</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Teresa Smith</span>
+            </div>
+            <p>Whatcom Transportation Authority</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Lisa Sohni</span>
+            </div>
+            <p>Opportunity Council</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Mariann Strachan</span>
+            </div>
+            <p>Community Volunteer</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">David Tseng</span>
+            </div>
+            <p>Samson Rope</p>
           </div>
         </div>
         <div class="fdc-column">
           <p class="red-text">Tuesday Panel Members</p>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Pat Atkinson</span>
+              <span class="fdc-title"> - Chair</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>Community Volunteer</p>
           </div>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Marty Buck</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>North Coast Credit Union</p>
           </div>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Lisa Elliot</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>Community Volunteer</p>
           </div>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Deidre Kane</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>Community Volunteer</p>
           </div>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Paula Kobos</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>Community Volunteer</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Joseph McClain</span>
+              <span class="fdc-title"> - Chair</span>
+            </div>
+            <p>Community Volunteer</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Michael McGowan</span>
+            </div>
+            <p>Community Volunteer</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Mark Moder</span>
+            </div>
+            <p>Whatcom Center for Early Learning</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Kim Reade</span>
+            </div>
+            <p>Washington Federal</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Ray Rodriguez</span>
+            </div>
+            <p>BP Cherry Point Refinery</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Harman Sandhu</span>
+            </div>
+            <p>US Bank</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Meagan Southworth</span>
+            </div>
+            <p>WECU</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Kathryn Weilage</span>
+            </div>
+            <p>Bellingham Public Schools</p>
           </div>
         </div>
         <div class="fdc-column">
           <p class="red-text">Wednesday Panel Members</p>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Natalie Boatman</span>
+              <span class="fdc-title"> - Chair</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>WECU</p>
           </div>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Cathy Archambault</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>Bellingham Public Schools</p>
           </div>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Susan Arrigoni</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>GeoEngineers</p>
           </div>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Kristi Birkeland</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>Q4Intelligence</p>
           </div>
           <div class="fdc-row">
             <div class="fdc-name-title">
-              <span class="fdc-name">Troy  Nutter</span>
-              <span class="fdc-title"> - fdc Chair</span>
+              <span class="fdc-name">Tawnya Bybee</span>
             </div>
-            <p>Puget Sound Energy, Operational Training Manager</p>
+            <p>US Bank</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Paula Cotton</span>
+            </div>
+            <p>BP Cherry Point Refinery</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Ginger Davis</span>
+            </div>
+            <p>US Bank</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Philip Jones</span>
+            </div>
+            <p>Community Volunteer</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Patricia Proctor</span>
+            </div>
+            <p>Whatcom County Government</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Monique Ransom</span>
+            </div>
+            <p>Larson Gross</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Justin Shoonover</span>
+            </div>
+            <p>Faithlife</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">Lindsay Schupbach</span>
+            </div>
+            <p>WECU</p>
+          </div>
+          <div class="fdc-row">
+            <div class="fdc-name-title">
+              <span class="fdc-name">BJ van Leeuwen</span>
+            </div>
+            <p>Bank of the Pacific</p>
           </div>
         </div>
       </div>
