0170fc5a60a733e3d381977f76e47413c08b08c9
luke <lukehtravis@gmail.com>
2018-08-22 12:04:55 -0700

    campaign tools mobile and desktop

---
+++ inc/other-campaign-tools.php
@@ -0,0 +1,38 @@
+<div class="page-container campaign-tools-container">
+  <section class="campaign-tools">
+    <div class="flex-title">
+      <div class="headline-left">
+        <h1>Campaign Tools</h1>
+        <p>Are you an Employee Campaign Coordinator (ECC) in need of some help? Below are some handy tools to help you run a successful <a href="#">workplace campaign</a>.</p>
+        <p>Want all of this on a thumb drive? No problem. <a href="#">Just ask</a>.</p>
+        <a href="#" class="a-button"><button class="orange-bg">Download 'Em All</button></a>
+      </div>
+      <div class="headline-right">
+        <img src="/assets/img/party-people-offmain.png" />
+      </div>
+    </div>
+    <div class="campaign-tools-box">
+      <div class="campaign-tools-section">
+        <h2>Campaign Materials</h2>
+        <p><a href="#">Brochure 2017</a></p>
+        <p><a href="#">Campaign Checklist</a></p>
+        <p><a href="#">Campaign Wrap Up Checklist</a></p>
+        <p><a href="#">Campaign Wrap up Sheet-pdf</a></p>
+        <p><a href="#">Campaign Wrap Up Sheet-Word</a></p>
+        <p><a href="#">CEO Letter of Support</a></p>
+        <p><a href="#">Pledge Form</a></p>
+        <p><a href="#">Poster for 2017 Campaign</a></p>
+        <p><a href="#">Poster for ALICE Infographic</a></p>
+        <p><a href="#">United Way Cash Back Flyer</a></p>
+        <p><a href="#">Why Give to United Way?</a></p>
+      </div>
+      <div class="campaign-tools-section">
+        <h2>Campaign Tips</h2>
+        <p><a href="#">Ask, Thank & Inform</a></p>
+        <p><a href="#">Campaign Coordinator Action Guide</a></p>
+        <p><a href="#">Fresh and Fun Campaign Ideas</a></p>
+        <p><a href="#">Low Cost Prize Idea-Office Coupons</a></p>
+      </div>
+    </div>
+  </section>
+</div>
