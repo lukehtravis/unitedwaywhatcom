281ca0f5ea2dc5d1e4e154070d6e5f827e2a81b7
luke <lukehtravis@gmail.com>
2018-08-22 11:01:50 -0700

    blog post desktop mobile

---
+++ inc/blog-post.php
@@ -0,0 +1,18 @@
+<div class="page-container blog-container">
+  <section class="blog">
+    <div class="offpage-headline">
+      <a href="#" class="back-to-list">< Back to All Posts</a>
+    </div>
+    <div class="blog-box">
+      <div class="post-header">
+        <h2>United Way Announces over $850k in Grants to Local Non-Profit Programs</h2>
+        <p>July 24, 2018</p>
+      </div>
+      <img src="/assets/img/blog-image.png" />
+      <p>Over 30 non-profit leaders convened at the Whatcom Center for Philanthropy where United Way of Whatcom County President/CEO Peter Theisen announced new grant awardees. After a competitive grant round in which United Way was seeking partners to live united against financial instability in Whatcom County, 11 local non-profit programs were selected for new one-year grants. This is in addition to 23 programs who are in the midst of previously awarded three-year grants.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean lacinia bibendum nulla sed consectetur.</p>
+      <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo. Aenean lacinia bibendum nulla sed consectetur.</p>
+      <p>Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec id elit non mi porta gravida at eget metus. Donec ullamcorper nulla non metus auctor fringilla. Curabitur blandit tempus porttitor. Sed posuere consectetur est at lobortis. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis euismod. Sed posuere consectetur est at lobortis. Donec sed odio dui. Donec id elit non mi porta gravida at eget metus. Donec id elit non mi porta gravida at eget metus.</p>
+    </div>
+  </section>
+</div>
